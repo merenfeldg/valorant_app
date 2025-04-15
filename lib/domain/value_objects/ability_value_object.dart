b29da54e48b14b3984cffd4018aff3642dca8ae3
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2025-04-15 14:44:13 -0300

    refactor: add variable image in AbilityValueObject

--- lib/domain/value_objects/ability_value_object.dart
+++ lib/domain/value_objects/ability_value_object.dart
@@ -1,9 +1,11 @@
 class AbilityValueObject {
   final String name;
+  final String image;
   final String descripton;
 
   AbilityValueObject({
     required this.name,
+    required this.image,
     required this.descripton, //
   });
 }
