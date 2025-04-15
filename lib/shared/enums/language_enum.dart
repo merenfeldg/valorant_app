fc95829ff802c3f97d0ee2aaf8d4a7d5e9d1e434
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2025-04-15 16:26:31 -0300

    refactor: add acronym variable in LanguageEnum

--- lib/shared/enums/language_enum.dart
+++ lib/shared/enums/language_enum.dart
@@ -1,34 +1,42 @@
 enum LanguageEnum {
   portuguese(
     name: 'Portuguese',
+    acronym: 'br',
     image: 'assets/images/flags/brazil_flag.png', //
   ),
   english(
     name: 'English',
+    acronym: 'en',
     image: 'assets/images/flags/eua_flag.png', //
   ),
   spanish(
     name: 'Spanish',
+    acronym: 'es',
     image: 'assets/images/flags/spain_flag.png', //
   ),
   chinese(
     name: 'Chinese',
+    acronym: 'zh-cn',
     image: 'assets/images/flags/china_flag.png', //
   ),
   france(
     name: 'France',
+    acronym: 'fr',
     image: 'assets/images/flags/france_flag.png', //
   ),
   german(
     name: 'German',
+    acronym: 'de',
     image: 'assets/images/flags/germany_flag.png', //
   );
 
   final String name;
+  final String acronym;
   final String image;
 
   const LanguageEnum({
     required this.name,
+    required this.acronym,
     required this.image, //
   });
 }
