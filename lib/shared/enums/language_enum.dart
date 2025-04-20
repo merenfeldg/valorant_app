ca3ac79a8f12e3d6c8660ecdb5b06519bcfbbd1a
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2025-04-20 09:18:32 -0300

    fix: change the images path of image in LanguageEnum to the right path

--- lib/shared/enums/language_enum.dart
+++ lib/shared/enums/language_enum.dart
@@ -2,32 +2,32 @@ enum LanguageEnum {
   portuguese(
     name: 'Portuguese',
     acronym: 'br',
-    image: 'assets/images/flags/brazil_flag.png', //
+    image: 'lib/ui/design_system/assets/images/flags/brazil_flag.png',
   ),
   english(
     name: 'English',
     acronym: 'en',
-    image: 'assets/images/flags/eua_flag.png', //
+    image: 'lib/ui/design_system/assets/images/flags/eua_flag.png',
   ),
   spanish(
     name: 'Spanish',
     acronym: 'es',
-    image: 'assets/images/flags/spain_flag.png', //
+    image: 'lib/ui/design_system/assets/images/flags/spain_flag.png',
   ),
   chinese(
     name: 'Chinese',
     acronym: 'zh-cn',
-    image: 'assets/images/flags/china_flag.png', //
+    image: 'lib/ui/design_system/assets/images/flags/china_flag.png',
   ),
   france(
     name: 'France',
     acronym: 'fr',
-    image: 'assets/images/flags/france_flag.png', //
+    image: 'lib/ui/design_system/assets/images/flags/france_flag.png',
   ),
   german(
     name: 'German',
     acronym: 'de',
-    image: 'assets/images/flags/germany_flag.png', //
+    image: 'lib/ui/design_system/assets/images/flags/germany_flag.png',
   );
 
   final String name;
