00c0faea4d9c19477736d6992f22a284b8f891e8
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2025-04-21 18:54:33 -0300

    feat: add VavaTextForm in Lib/Ui/Design_system/Forms

---
+++ lib/ui/design_system/widgets/forms/vava_text_form.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:valorant_app/ui/design_system/themes/theme_extensions.dart';
+
+class VavaTextForm extends StatelessWidget {
+  //
+  final void Function(String) onChanged;
+
+  const VavaTextForm({
+    super.key,
+    required this.onChanged, //
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.colors;
+
+    return SizedBox(
+      height: 45,
+      child: TextFormField(
+        style: TextStyle(color: colors.greyLightColor),
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: colors.greyDarkColor,
+          prefixIcon: Padding(
+            padding: const EdgeInsets.only(left: 30, right: 10),
+            child: Icon(
+              Icons.search,
+              color: colors.greyLightColor, //
+            ),
+          ),
+          hintText: 'Search for agent...', //
+          hintStyle: TextStyle(
+            color: colors.greyLightColor, //
+            fontSize: 12,
+          ),
+          contentPadding: EdgeInsets.symmetric(vertical: 10),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(40),
+            borderSide: BorderSide(style: BorderStyle.none),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(40),
+            borderSide: BorderSide(
+              style: BorderStyle.none, //
+            ),
+          ),
+          isDense: true,
+        ),
+      ),
+    );
+  }
+}
