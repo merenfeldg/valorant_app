fd096bbf26efa79745a81c7d81927513d3255366
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2025-04-21 14:03:06 -0300

    fix: moving the assets folder out of the Lib folder

--- lib/shared/enums/language_enum.dart
+++ lib/shared/enums/language_enum.dart
@@ -2,32 +2,32 @@ enum LanguageEnum {
   portuguese(
     name: 'Portuguese',
     acronym: 'br',
-    image: 'lib/ui/design_system/assets/images/flags/brazil_flag.png',
+    image: 'assets/images/flags/brazil_flag.png',
   ),
   english(
     name: 'English',
     acronym: 'en',
-    image: 'lib/ui/design_system/assets/images/flags/eua_flag.png',
+    image: 'assets/images/flags/eua_flag.png',
   ),
   spanish(
     name: 'Spanish',
     acronym: 'es',
-    image: 'lib/ui/design_system/assets/images/flags/spain_flag.png',
+    image: 'assets/images/flags/spain_flag.png',
   ),
   chinese(
     name: 'Chinese',
     acronym: 'zh-cn',
-    image: 'lib/ui/design_system/assets/images/flags/china_flag.png',
+    image: 'assets/images/flags/china_flag.png',
   ),
   france(
     name: 'France',
     acronym: 'fr',
-    image: 'lib/ui/design_system/assets/images/flags/france_flag.png',
+    image: 'assets/images/flags/france_flag.png',
   ),
   german(
     name: 'German',
     acronym: 'de',
-    image: 'lib/ui/design_system/assets/images/flags/germany_flag.png',
+    image: 'assets/images/flags/germany_flag.png',
   );
 
   final String name;
