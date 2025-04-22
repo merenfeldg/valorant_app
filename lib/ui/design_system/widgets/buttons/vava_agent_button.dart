76e4a376159a12d5e7e52edfb76239c0e1ca5e01
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2025-04-22 20:10:02 -0300

    refactor: add InkWell in VavaAgentButtons in Lib/Ui/Design_system/Widgets/Buttons

--- lib/ui/design_system/widgets/buttons/vava_agent_button.dart
+++ lib/ui/design_system/widgets/buttons/vava_agent_button.dart
@@ -7,32 +7,38 @@ class VavaAgentButton extends StatelessWidget {
   final String text;
   final String image;
   final String? icon;
+  final VoidCallback onTap;
 
   const VavaAgentButton._({
     required this.text,
     required this.image,
     required this.icon,
+    required this.onTap,
   });
 
   factory VavaAgentButton.fullBody({
     required String text,
     required String image,
     required String icon,
+    required VoidCallback onTap,
   }) {
     return VavaAgentButton._(
       text: text,
       image: image,
       icon: icon, //
+      onTap: onTap,
     );
   }
   factory VavaAgentButton.portrait({
     required String text,
     required String image,
+    required VoidCallback onTap,
   }) {
     return VavaAgentButton._(
       text: text,
       image: image,
       icon: null, //
+      onTap: onTap,
     );
   }
 
@@ -44,11 +50,13 @@ class VavaAgentButton extends StatelessWidget {
         ? _ButtonFullBody(
           text: text,
           image: image,
-          icon: icon!, //
+          icon: icon!,
+          onTap: onTap, //
         )
         : _ButtonPortrait(
           text: text,
-          image: image, //
+          image: image,
+          onTap: onTap, //
         );
   }
 }
@@ -57,11 +65,13 @@ class _ButtonFullBody extends StatelessWidget {
   final String text;
   final String image;
   final String icon;
+  final VoidCallback onTap;
 
   const _ButtonFullBody({
     required this.text,
     required this.image,
     required this.icon,
+    required this.onTap,
   });
 
   @override
@@ -69,45 +79,48 @@ class _ButtonFullBody extends StatelessWidget {
     final colors = context.colors;
     final isTextMoreFiveCharacter = text.length > 5;
 
-    return Container(
-      height: 150,
-      width: 150, //
-      decoration: BoxDecoration(
-        color: colors.redPrimaryColor, //
-        borderRadius: BorderRadius.circular(24),
-      ),
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Positioned(
-            bottom: -10,
-            right: -18,
-            child: CachedNetworkImage(
-              imageUrl: image, //
-              height: 200,
-            ),
-          ),
-          Positioned(
-            top: 110,
-            left: isTextMoreFiveCharacter ? 97 : 120,
-            child: Text(
-              text,
-              style: TextStyle(
-                color: colors.whiteColor,
-                fontSize: isTextMoreFiveCharacter ? 18 : 24, //
-                fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 150,
+        width: 150, //
+        decoration: BoxDecoration(
+          color: colors.redPrimaryColor, //
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Positioned(
+              bottom: -10,
+              right: -18,
+              child: CachedNetworkImage(
+                imageUrl: image, //
+                height: 200,
               ),
-            ), //
-          ),
-          Positioned(
-            top: -15,
-            left: 132,
-            child: Image.asset(
-              icon, //
-              height: 36,
-            ), //
-          ),
-        ],
+            ),
+            Positioned(
+              top: 110,
+              left: isTextMoreFiveCharacter ? 97 : 120,
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: colors.whiteColor,
+                  fontSize: isTextMoreFiveCharacter ? 18 : 24, //
+                  fontWeight: FontWeight.bold,
+                ),
+              ), //
+            ),
+            Positioned(
+              top: -15,
+              left: 132,
+              child: Image.asset(
+                icon, //
+                height: 36,
+              ), //
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -116,39 +129,44 @@ class _ButtonFullBody extends StatelessWidget {
 class _ButtonPortrait extends StatelessWidget {
   final String text;
   final String image;
+  final VoidCallback onTap;
 
   const _ButtonPortrait({
     required this.text,
-    required this.image, //
+    required this.image,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
     final colors = context.colors;
 
-    return Container(
-      height: 130,
-      width: 100,
-      decoration: BoxDecoration(
-        color: colors.greyNormalColor,
-        borderRadius: BorderRadius.circular(24),
-      ),
-      child: Column(
-        children: [
-          Gap(8),
-          Image.network(
-            image,
-            height: 80, //
-          ),
-          Gap(8),
-          Text(
-            text,
-            style: TextStyle(
-              color: colors.whiteColor,
-              fontSize: 16, //
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 130,
+        width: 100,
+        decoration: BoxDecoration(
+          color: colors.greyNormalColor,
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Column(
+          children: [
+            Gap(8),
+            Image.network(
+              image,
+              height: 80, //
             ),
-          ),
-        ],
+            Gap(8),
+            Text(
+              text,
+              style: TextStyle(
+                color: colors.whiteColor,
+                fontSize: 16, //
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
